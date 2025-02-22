2339c4533aab6440a83295a27521fe0822bdc44f
Jonathon Moore <jonathon.moore@yum.com>
2025-02-22 12:38:01 -0800

    Implemented response streaming for improved performance

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,5 @@ import { fetchAllEvents } from '$lib/server/remote-events';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-    const loadedEvents = await fetchAllEvents();
-    return {
-        events: loadedEvents
-    }
+    return { events: fetchAllEvents() }
 };
